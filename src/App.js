7e8925738107079a28e12d065c9ee62a06d0814f
IAmAditya <aditya_ranjan@trisysit.com>
2023-01-22 13:02:21 +0530

    get contacts added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,65 @@
 /** @format */
 
 import {
-  Routes,
-  Route,
-  useNavigationType,
-  useLocation,
-  BrowserRouter as Router
+	Routes,
+	Route,
+	useNavigationType,
+	useLocation,
+	BrowserRouter as Router,
 } from "react-router-dom";
 import Homepage2 from "./pages/Homepage2";
 import { useEffect } from "react";
-import Navbar from "./pages/Navbar/Navbar"
+import Navbar from "./pages/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import GetInTouch from "./components/GetInTouch/GetInTouch";
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
+	const action = useNavigationType();
+	const location = useLocation();
+	const pathname = location.pathname;
 
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
+	useEffect(() => {
+		if (action !== "POP") {
+			window.scrollTo(0, 0);
+		}
+	}, [action, pathname]);
 
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
+	useEffect(() => {
+		let title = "";
+		let metaDescription = "";
 
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-    }
+		switch (pathname) {
+			case "/":
+				title = "";
+				metaDescription = "";
+				break;
+		}
 
-    if (title) {
-      document.title = title;
-    }
+		if (title) {
+			document.title = title;
+		}
 
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
+		if (metaDescription) {
+			const metaDescriptionTag = document.querySelector(
+				'head > meta[name="description"]'
+			);
+			if (metaDescriptionTag) {
+				metaDescriptionTag.content = metaDescription;
+			}
+		}
+	}, [pathname]);
 
-  return (
-    <>
-      <Navbar />
+	return (
+		<>
+			{/* <Navbar />
       <Routes>
         <Route path="/" element={<Footer />} />
-      </Routes>
-    </>
-  );
+      </Routes> */}
+
+			<Navbar />
+			<GetInTouch />
+			<Footer />
+		</>
+	);
 }
 export default App;
--- src/components/GetInTouch/GetInTouch.js
+++ src/components/GetInTouch/GetInTouch.js
@@ -1,11 +1,29 @@
 /** @format */
 
 import React from "react";
-import BgImage from "../../../public/rectangle-44@2x.png";
+import BgImage from "../../Assets/rectangle-44@2x.png";
+import BgImage2 from "../../Assets/pexelsemrecanacer2079293-3@2x.png";
+import "./GetInTouch.css";
 const GetInTouch = () => {
 	return (
-		<div>
+		<div style={{ marginTop: "150px" }} className="get-main-container">
 			<img className="get-bg-img" alt="" src={BgImage} />
+			<img className="get-bg-img2" alt="" src={BgImage2} />
+			<div className="get-title">Get in touch</div>
+			<div className="get-dont-miss">
+				<div className="get-dont-miss-text">
+					Don’t miss any update about new offers & Discounts
+				</div>
+			</div>
+			<form className="get-form">
+				<div className="get-input-container" />
+				<button className="get-btn-container">
+					<div className="get-btn" />
+					<b className="subscribe">Subscribe</b>
+				</button>
+				<input placeholder="Enter Your Email Address" className="get-input" />
+				<img className="get-mail-icon" alt="" src="../group-1000003862.svg" />
+			</form>
 		</div>
 	);
 };
